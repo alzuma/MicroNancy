6aa46d1d8bc7e4b09ddda5a54135c98806b64014
Kristaps <kristaps.vitolins@gmail.com>
2016-07-20 22:14:42 +0300

    Add user for request

--- JustDoIt/GateWay/app/Bootstrapper.cs
+++ JustDoIt/GateWay/app/Bootstrapper.cs
@@ -1,5 +1,9 @@
-﻿using Nancy.Bootstrapper;
+﻿using GateWay.server.authentication.components;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Owin;
 using Nancy.TinyIoc;
+using System.Security.Claims;
 
 namespace GateWay.app
 {
@@ -13,6 +17,19 @@ namespace GateWay.app
             {
                 return string.Concat("pub/", viewName);
             });
+        }
+
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            base.RequestStartup(container, pipelines, context);
+
+            var owinEnvironment = context.GetOwinEnvironment();
+
+            var principal = owinEnvironment?["server.User"] as ClaimsPrincipal;
+
+            if (principal == null) return;
+
+            context.CurrentUser = new GateWayUser(principal.Identity.Name, principal.Claims);
         }
     }
 }
---
+++ JustDoIt/GateWay/server/authentication/components/GateWayUser.cs
@@ -0,0 +1,27 @@
+﻿using Nancy.Security;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace GateWay.server.authentication.components
+{
+    public class GateWayUser : IUserIdentity
+    {
+        private readonly string _userName;
+        private readonly List<string> _claims;
+        public GateWayUser(string userName, IEnumerable<Claim> claims)
+        {
+            _userName = userName;
+            _claims = new List<string>();
+            foreach (var claim in claims)
+            {
+                if (claim.Type == ClaimTypes.Role)
+                {
+                    _claims.Add(claim.Value);
+                }
+            }
+        }
+
+        public IEnumerable<string> Claims => _claims;
+        public string UserName => _userName;
+    }
+}
