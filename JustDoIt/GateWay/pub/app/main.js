33748224cb170e89fdee461c8018c640892a1a55
Kristaps <kristaps.vitolins@gmail.com>
2016-07-25 23:20:01 +0300

    remove gen stuff

--- JustDoIt/GateWay/pub/app/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_component_1 = require('./app.component');
-var app_routes_1 = require('./app.routes');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [app_routes_1.appRouterProviders]);
-//# sourceMappingURL=main.js.map
